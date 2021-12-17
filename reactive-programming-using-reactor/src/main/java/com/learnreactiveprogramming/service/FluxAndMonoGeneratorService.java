66c0f0a652f0dbe3c26bc344c0987374c6400d0b
lykismekk <lykismekk@outlook.com>
2021-12-17 11:25:59 +0800

    simple flux implementation

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,20 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("alex", "tom", "tim"));
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService t = new FluxAndMonoGeneratorService();
+        // we can having the flux data, only when we subscribe it
+        t.namesFlux()
+                .subscribe(n -> System.out.println("Name is " + n));
+    }
+
 }
