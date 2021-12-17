8159f13e3811300b2d68ba501f45e22950180f9e
lykismekk <lykismekk@outlook.com>
2021-12-17 12:23:28 +0800

    simple Mono implementation

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,6 +1,7 @@
 package com.learnreactiveprogramming.service;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -10,11 +11,18 @@ public class FluxAndMonoGeneratorService {
         return Flux.fromIterable(List.of("alex", "tom", "tim"));
     }
 
+    public Mono<String> nameMono() {
+        return Mono.just("patrick");
+    }
+
     public static void main(String[] args) {
         FluxAndMonoGeneratorService t = new FluxAndMonoGeneratorService();
-        // we can having the flux data, only when we subscribe it
+        // we can having the Flux data, only when we subscribe it
         t.namesFlux()
                 .subscribe(n -> System.out.println("Name is " + n));
+        // same as the Mono data
+        t.nameMono()
+                .subscribe(n -> System.out.println("Mono name is " + n));
     }
 
 }
