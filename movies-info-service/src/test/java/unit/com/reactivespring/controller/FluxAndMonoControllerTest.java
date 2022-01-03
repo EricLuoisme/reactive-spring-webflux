ca34810c728a0b668bfedae76b580ecdcc2a8c3a
lykismekk <lykismekk@outlook.com>
2022-01-03 16:01:44 +0800

    adding simple flux Unit test by using the webTestClient

--- movies-info-service/src/test/java/unit/com/reactivespring/controller/FluxAndMonoControllerTest.java
+++ movies-info-service/src/test/java/unit/com/reactivespring/controller/FluxAndMonoControllerTest.java
@@ -6,6 +6,10 @@ import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWeb
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,8 +22,11 @@ class FluxAndMonoControllerTest {
     @Autowired
     WebTestClient webTestClient;
 
+    /**
+     * Approach 1, use webTestClient and expect all the stuff with it
+     */
     @Test
-    void flux() {
+    void flux_approach1() {
         webTestClient.get()
                 .uri("/flux")
                 .exchange()
@@ -29,11 +36,36 @@ class FluxAndMonoControllerTest {
                 .hasSize(3);
     }
 
+    /**
+     * Approach 2, use webTestClient to receive the http response, then us StepVerifier to verify the response
+     */
     @Test
-    void mono() {
+    void flux_approach2() {
+        var fluxResp = webTestClient.get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(fluxResp)
+                .expectNext(1, 2, 3)
+                .verifyComplete();
     }
 
     @Test
-    void stream() {
+    void flux_approach3() {
+        webTestClient.get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(Integer.class)
+                .consumeWith(res -> {
+                    List<Integer> responseBody = res.getResponseBody();
+                    assert Objects.requireNonNull(responseBody).size() == 3;
+                });
     }
+
 }
