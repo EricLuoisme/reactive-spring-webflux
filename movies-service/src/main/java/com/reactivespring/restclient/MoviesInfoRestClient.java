9d229eb7065dcd00dcc2c54b6dfeb55ef746c899
lykismekk <lykismekk@outlook.com>
2022-05-10 21:59:36 +0800

    adding error response handling in mono and flux

--- movies-service/src/main/java/com/reactivespring/restclient/MoviesInfoRestClient.java
+++ movies-service/src/main/java/com/reactivespring/restclient/MoviesInfoRestClient.java
@@ -1,8 +1,11 @@
 package com.reactivespring.restclient;
 
 import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.exception.MoviesInfoClientException;
+import com.reactivespring.exception.MoviesInfoServerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -24,6 +27,29 @@ public class MoviesInfoRestClient {
         return webClient.get()
                 .uri(url, movieId)
                 .retrieve()
+                // take care of exception (error resp)
+                .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
+                    if (clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)) {
+                        return Mono.error(
+                                new MoviesInfoClientException("There is no MovieInfo available for the passed id: " + movieId,
+                                        clientResponse.statusCode().value()));
+                    }
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(responseMsg ->
+                                    Mono.error(
+                                            new MoviesInfoClientException(responseMsg, clientResponse.statusCode().value())));
+                })
+                .onStatus(HttpStatus::is5xxServerError, clientResponse -> {
+                    if (clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)) {
+                        return Mono.error(
+                                new MoviesInfoServerException("Server Exception in MovieInfo service : " + clientResponse));
+                    }
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(responseMsg ->
+                                    Mono.error(
+                                            new MoviesInfoClientException(responseMsg, clientResponse.statusCode().value())));
+                })
+                // take care of normal result
                 .bodyToMono(MovieInfo.class)
                 .log();
     }
