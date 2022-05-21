b51f3fb12e98161e74664cc295b8dce4a03f096e
lykismekk <lykismekk@outlook.com>
2022-05-21 15:48:04 +0800

    adding sink for server event emission

---
+++ movies-info-service/src/test/java/intg/com/reactivespring/controller/SinkTest.java
@@ -0,0 +1,34 @@
+package com.reactivespring.controller;
+
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+
+public class SinkTest {
+
+    @Test
+    public void sink() {
+
+        Sinks.Many<Integer> replaySink = Sinks.many().replay().all();
+
+        replaySink.emitNext(1, Sinks.EmitFailureHandler.FAIL_FAST);
+        replaySink.emitNext(2, Sinks.EmitFailureHandler.FAIL_FAST);
+
+        Flux<Integer> integerFlux = replaySink.asFlux();
+        integerFlux.subscribe(i -> {
+            System.out.println("Subscriber 1: " + i);
+        });
+
+        Flux<Integer> integerFlux_2 = replaySink.asFlux();
+        integerFlux_2.subscribe(i -> {
+            System.out.println("Subscriber 2: " + i);
+        });
+
+        // another emitting
+        System.out.println("\n Another emission occur \n");
+        replaySink.emitNext(3, Sinks.EmitFailureHandler.FAIL_FAST);
+
+    }
+
+}
