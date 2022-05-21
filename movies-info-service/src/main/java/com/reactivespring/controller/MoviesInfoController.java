3d5be9dd68c8a85bf6a078eda312d09ad2177e65
lykismekk <lykismekk@outlook.com>
2022-05-21 17:18:53 +0800

    finished the whole stuff

--- movies-info-service/src/main/java/com/reactivespring/controller/MoviesInfoController.java
+++ movies-info-service/src/main/java/com/reactivespring/controller/MoviesInfoController.java
@@ -2,12 +2,15 @@ package com.reactivespring.controller;
 
 import com.reactivespring.domain.MovieInfo;
 import com.reactivespring.service.MoviesInfoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
 
 import javax.validation.Valid;
 
@@ -15,12 +18,15 @@ import javax.validation.Valid;
  * For the WebFlux response, if we put noting (not specifically put @ResponseStatus),
  * even an error occur inside, we still response on HttpStatus:2xx
  */
+@Slf4j
 @RestController
 @RequestMapping("/v1")
 public class MoviesInfoController {
 
     @Autowired
     private MoviesInfoService moviesInfoService;
+
+    private Sinks.Many<MovieInfo> moviesInfoSink = Sinks.many().replay().all();
 
 
     @GetMapping("/movieinfos")
@@ -35,8 +41,8 @@ public class MoviesInfoController {
                 .log();
     }
 
-    @GetMapping("/movieinfos")
-    public Mono<MovieInfo> getAllMovieInfos(@RequestParam(value = "name", required = false) String name) {
+    @GetMapping("/movieinfos_2")
+    public Mono<MovieInfo> getAllMovieInfos_2(@RequestParam(value = "name", required = false) String name) {
         return moviesInfoService.getMovieInfoByName(name)
                 .log();
     }
@@ -59,10 +65,27 @@ public class MoviesInfoController {
                 .log();
     }
 
+
+    @GetMapping(value = "/movieinfos/streams", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Flux<MovieInfo> getAllMovieInfosStream() {
+        return moviesInfoSink.asFlux();
+    }
+
     @PostMapping("/movieinfos")
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<MovieInfo> addMovieInfo(@RequestBody @Valid MovieInfo movieInfo) {
         return moviesInfoService.addMovieInfo(movieInfo)
+                // after the Consumer has consume it
+                // just adding it into the sink
+                .doOnNext(saveInfo -> {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    log.info(">>> Start adding into the Sink");
+                    moviesInfoSink.tryEmitNext(saveInfo);
+                })
                 .log();
     }
 
